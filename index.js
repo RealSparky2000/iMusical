b721cbbe2de8a92e2fda9b95978c1051c7497968
RealSparky2000 <38855749+RealSparky2000@users.noreply.github.com>
2018-12-08 13:09:37 +0000

    Update index.js

--- index.js
+++ index.js
@@ -19,5 +19,41 @@ client.on('disconnect', () => console.log('I just disconnected, making sure you 
 client.on('reconnecting', () => console.log('I am reconnecting now!'));
 
 client.on('message', async msg => { 
+  if (msg.author.bot) return undefined;
+
+	if (!msg.content.startsWith(PREFIX)) return undefined;
+
+	const args = msg.content.split(' ');
+
+	const searchString = args.slice(1).join(' ');
+
+	const url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
+
+	const serverQueue = queue.get(msg.guild.id);
+
+	let command = msg.content.toLowerCase().split(' ')[0];
+
+	command = command.slice(PREFIX.length)
+
+	if (command === 'play') {
+
+		const voiceChannel = msg.member.voiceChannel;
+
+		if (!voiceChannel) return msg.channel.send('I\'m sorry but you need to be in a voice channel to play music!');
+
+		const permissions = voiceChannel.permissionsFor(msg.client.user);
+
+		if (!permissions.has('CONNECT')) {
+
+			return msg.channel.send('I cannot connect to your voice channel, make sure I have the proper permissions!');
+
+		}
+
+		if (!permissions.has('SPEAK')) {
+
+			return msg.channel.send('I cannot speak in this voice channel, make sure I have the proper permissions!');
+
+		}
+   } 
 }) 
 client.login(process.env.BOT_TOKEN)
